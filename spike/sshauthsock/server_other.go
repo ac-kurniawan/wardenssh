c34c7707694e594b2ed903e2f05b00e8e3f75113
ac-kurniawan <ac.kurniawan99@gmail.com>
2026-08-15 15:22:54 +0700

    chore: delete spike

--- spike/sshauthsock/server_other.go
+++
@@ -1,6 +0,0 @@
-//go:build !windows
-
-package sshauthsock
-
-// On non-Windows platforms, the named-pipe spike is not applicable.
-// Unix domain sockets are used instead (see internal/sshagent/listen_unix.go).
